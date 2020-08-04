0ce2734d7b6053c1f9163822d6af8bcfce68ed0e
janika <jkiss12@gmail.com>
2020-08-04 17:59:58 +0100

    Created Java Project in Eclipse

---
+++ BankingApplication/src/BankingApplication.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author janika
+ *
+ */
+public class BankingApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
